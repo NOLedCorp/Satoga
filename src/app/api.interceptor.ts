9164b321690e937dde4efa2b9dc2dd10ae3d17c7
Ivan Nomokonov <cpwxt38@dev.vtb>
2019-08-06 11:13:55 +0300

    fix

--- src/app/api.interceptor.ts
+++ src/app/api.interceptor.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor{
     intercept(req:HttpRequest<any>, next:HttpHandler): Observable<HttpEvent<any>>{
-        if (req.url.includes('admin_controller.php') && !req.url.includes('Key=enter')) {
+        if (req.url.includes('admin_controller.php') && JSON.parse(sessionStorage.getItem('satogaUserKey')) && !req.url.includes('Key=enter')) {
             let u=JSON.parse(sessionStorage.getItem('satogaUserKey'));
             const paramReq = req.clone({
                 params: req.params.set('Login', encodeURIComponent(u.Login)).set('Password', encodeURIComponent(u.Password))
