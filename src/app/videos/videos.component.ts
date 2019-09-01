da3d1bcdc2e2a7b8274bd31473b7e226be345447
IANomokonov <nomokonov.vana@yandex.ru>
2019-09-01 12:54:22 +0300

    поправил видео и меню

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { SatogaService } from '../services/satoga.service';
 import { LoadService } from '../services/load.service';
@@ -7,31 +7,19 @@ import { ModalService } from '../services/modal.service';
 
 @Component({
   selector: 'app-videos',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './videos.component.html',
   styleUrls: ['./videos.component.less']
 })
 export class VideosComponent implements OnInit {
-  videos:any = [
-    // {
-    //   Path: 'https://youtu.be/uGBZ_--HPcM'
-    // },
-    // {
-    //   Path: 'https://youtu.be/uGBZ_--HPcM'
-    // },
-    // {
-    //   Path: 'https://youtu.be/uGBZ_--HPcM'
-    // },
-    // {
-    //   Path: 'https://youtu.be/uGBZ_--HPcM'
-    // },
-    // {
-    //   Path: 'https://youtu.be/uGBZ_--HPcM'
-    // },
-    // {
-    //   Path: 'https://youtu.be/uGBZ_--HPcM'
-    // }
-  ]
-  constructor(public snt:DomSanitizer, public ms:ModalService, private ss:SatogaService, private ls:LoadService, public us:UserService) { }
+  videos:any = [];
+  show = true;
+  constructor(public snt:DomSanitizer, 
+    public ms:ModalService, 
+    private ss:SatogaService, 
+    private ls:LoadService, 
+    public us:UserService,
+    private changeDetection: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.ls.showLoad = true;
@@ -44,6 +32,7 @@ export class VideosComponent implements OnInit {
       this.videos.forEach(v => {
         v.Path = v.Path.replace('youtu.be/','www.youtube.com/embed/');
       })
+      this.changeDetection.detectChanges();
       
       this.ls.showLoad=false;
     })
